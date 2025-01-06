d0997c0cf6adcdce0ac1e15993195f491ae1bf76
Hafeezullah-khattak <23pwbcs1003@uetpeshawar.edu.pk>
2025-01-06 19:51:33 +0500

    general commit

--- index.js
+++ index.js
@@ -21,12 +21,12 @@ mongoose
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
-  .then(() => console.log("🌟 Connected to MongoDB!"))
-  .catch((err) => console.error("❌ MongoDB connection error:", err));
+  .then(() => console.log(" Connected to MongoDB!"))
+  .catch((err) => console.error(" MongoDB connection error:", err));
 
 // Routes
 app.use("/api", authRoutes);
 
 // Start the server
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(` Server running on port ${PORT}`));
