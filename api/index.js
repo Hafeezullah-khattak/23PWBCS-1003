5eccdadd36d378ba2fbcb0ada5a4d7af20633e91
Hafeezullah-khattak <23pwbcs1003@uetpeshawar.edu.pk>
2025-01-06 23:40:13 +0500

    Prepare for deployment

--- api/index.js
+++ api/index.js
@@ -15,10 +15,7 @@ app.use(cors());
 
 // MongoDB Connection
 mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(process.env.MONGO_URI)
   .then(() => {
     console.log("Connected to MongoDB!");
     // Start the server after successful MongoDB connection
