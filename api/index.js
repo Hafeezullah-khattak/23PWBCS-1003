cc46197d2b19600a49862bf6eb06d245b927b17a
Hafeezullah-khattak <23pwbcs1003@uetpeshawar.edu.pk>
2025-01-09 17:59:19 +0500

    Prepare for deployment

--- api/index.js
+++ api/index.js
@@ -18,10 +18,6 @@ mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
     console.log("Connected to MongoDB!");
-    // Start the server after successful MongoDB connection
-    app.listen(5000, () => {
-      console.log("Server running on port 5000");
-    });
   })
   .catch((err) => {
     console.error("MongoDB connection error:", err);
